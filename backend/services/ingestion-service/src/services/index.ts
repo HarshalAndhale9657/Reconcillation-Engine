4b93693c47b70f9cbbb6c95cf59ae8164f70b262
Apurvborhade <apurvaborhadee@gmail.com>
2026-01-11 18:38:01 +0530

    Shutdown ingestion

--- backend/services/ingestion-service/src/services/index.ts
+++ backend/services/ingestion-service/src/services/index.ts
@@ -1,10 +1,17 @@
 import { logAppTx } from './appTx'
 import { logGatewayTx } from './gatewayTx'
 import { logBankTx } from './bankTx'
+
 async function run() {
     setTimeout(logAppTx, 1000);
     setTimeout(logGatewayTx, 2000);
     setTimeout(logBankTx, 3000);
+
+    // Exit process after 5 minutes (300000 ms)
+    setTimeout(() => {
+        console.log('Shutting down after 5 minutes');
+        process.exit(0);
+    }, 300000);
 }
 
 run()
